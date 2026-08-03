83a9eabfe812b50db7d05d7d951f74676c0fa092
Zachary Vacura <zvacura@netflix.com>
2026-08-03 19:57:41 -0400

    use symlink for /opt/pulsesecure

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,6 +14,9 @@ setfacl -d -m o::r /var/lib/pulsesecure/pulse
 systemctl enable /lib/systemd/system/pulsesecure.service
 /opt/pulsesecure/bin/setup_cef.sh install
 
+rm -rf /opt/pulsesecure
+ln -s /var/opt/pulsesecure /opt/pulsesecure
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
